334429c0f2890536e718320ed5dbeb7cb13f8e01
codyer.yi <codyer.yi@chinaredstar.com>
2016-08-12 18:40:59 +0800

    change package add contex to vm

--- foundation/src/main/java/com/redstar/foundation/viewmodel/ViewModel.java
+++ foundation/src/main/java/com/redstar/foundation/viewmodel/ViewModel.java
@@ -1,10 +1,17 @@
 package com.redstar.foundation.viewmodel;
 
+import android.content.Context;
+
 /**
  * Created by cody.yi on 2016/8/4.
  */
 public class ViewModel implements ViewListener {
     public Object mTag = "TAG";
+    public Context mContext;
+
+    public ViewModel(Context context) {
+        mContext = context;
+    }
 
     @Override
     public void onCreate() {
